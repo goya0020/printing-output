0859074eac62f3f1c06bed65e257a9b9c1050a36
Ritish <122370065+goya0020@users.noreply.github.com>
2023-01-17 09:32:22 -0500

    JS code added to JS file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
+let a = 3;
+let b = 5;
+let c;
+
+let output = 'let a =3;\n'
+output += 'let b = 5;\n';
+output += 'let c;\n';
+output += '---------\n';
+
+alert(output);
+
+/*
+let sum = a+b;
+let sub = a-b;
+let mul = a*b;
+
+// alert()
 
