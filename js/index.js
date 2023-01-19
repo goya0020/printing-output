c9c0e1a288c72c8a1f386cf204b7afbd749d1d48
Ritish <122370065+goya0020@users.noreply.github.com>
2023-01-19 16:26:42 -0500

    Printing-Output-Task-1

--- js/index.js
+++ js/index.js
@@ -2,13 +2,28 @@ let a = 3;
 let b = 5;
 let c;
 
-let output = 'let a =3;\n';
+let output = 'let a = 3;\n';
 output += 'let b = 5;\n';
 output += 'let c;\n';
-output += '---------\n';
+output += '-----------\n';
+output += 'a + b = ' + (a + b) + "\n";
+output += 'a - b = ' + (a - b) + "\n";
+output += 'a * b = ' + (a * b) + "\n";
+output += 'a / b = ' + (a / b) + "\n";
+output += 'a % b = ' + (a % b) + "\n";
+output += 'a += b = ' + (a += b) + "\n";
+output += 'a -= b = ' + (a -= b) + "\n";
+output += 'a *= b = ' + (a *= b) + "\n";
+output += 'a /= b = ' + (a /= b) + "\n";
+output += 'a %= b = ' + (a %= b) + "\n";
+output += 'a == b = ' + (a == b) + "\n";
+output += 'a != b = ' + (a != b) + "\n";
+output += 'a > b = ' + (a > b) + "\n";
+output += 'a < b = ' + (a < b) + "\n";
+output += '!a && !c = ' + (!a && !c) + "\n";
+output += '!a || !c = ' + (!a || !c) + "\n";
 
 alert(output);
-
 
 
 
